6519f9984ab39353a9fa2ec840c562bd73a39e30
qinxinren1 <qinxin.ren18@gmail.com>
2023-12-29 11:00:40 +0100

    part 1-4

---
+++ VolVis_Project_Framework/out/build/x64-Debug/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: D:/RQX/TUD/Q2/DV/VolVis_Project_Framework
+# Build directory: D:/RQX/TUD/Q2/DV/VolVis_Project_Framework/out/build/x64-Debug
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("integrity_tests")
---
+++ VolVis_Project_Framework/out/build/x64-Debug/integrity_tests/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: D:/RQX/TUD/Q2/DV/VolVis_Project_Framework/integrity_tests
+# Build directory: D:/RQX/TUD/Q2/DV/VolVis_Project_Framework/out/build/x64-Debug/integrity_tests
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
